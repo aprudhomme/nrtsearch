fa21ce20de678c86b001b8800e18565e02c34ae5
Andrew Prudhomme <asp@yelp.com>
2022-06-08 11:17:00 -0700

    Add incremental snapshot/restore/list/delete commands

--- grpc-gateway/luceneserver.pb.gw.go
+++ grpc-gateway/luceneserver.pb.gw.go
@@ -15,7 +15,6 @@ import (
 
 	"github.com/golang/protobuf/descriptor"
 	"github.com/golang/protobuf/proto"
-	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/grpc-ecosystem/grpc-gateway/utilities"
 	"google.golang.org/grpc"
@@ -23,6 +22,7 @@ import (
 	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 // Suppress "imported and not used" errors
@@ -1429,7 +1429,7 @@ func local_request_LuceneServer_Ready_1(ctx context.Context, marshaler runtime.M
 }
 
 func request_LuceneServer_Metrics_0(ctx context.Context, marshaler runtime.Marshaler, client LuceneServerClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq empty.Empty
+	var protoReq emptypb.Empty
 	var metadata runtime.ServerMetadata
 
 	msg, err := client.Metrics(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -1438,7 +1438,7 @@ func request_LuceneServer_Metrics_0(ctx context.Context, marshaler runtime.Marsh
 }
 
 func local_request_LuceneServer_Metrics_0(ctx context.Context, marshaler runtime.Marshaler, server LuceneServerServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq empty.Empty
+	var protoReq emptypb.Empty
 	var metadata runtime.ServerMetadata
 
 	msg, err := server.Metrics(ctx, &protoReq)
